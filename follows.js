b57bbd310bfba149ecf8aa1eb0a157ea201f671e
gmza <alex@wsmp.co.za>
2013-11-19 20:46:32 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -2,7 +2,7 @@
 	
 	var defaultKey		= 'XuJJYA3mJfVUzXaeL9BahVTT05SdUWiB2zJ6UcmywsonXmzB', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1200180497], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'TmpViz', // Replaces Xively logo in the header
+		applicationName	= 'Eico', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
